e3d4c4f57a61d7287bab2abf709ecc26f5496b50
pedroduarte0 <63309559+pedroduarte0@users.noreply.github.com>
2025-01-04 12:02:56 +0000

    #008 - Add Seq.

--- DotNetDataAccessTour.AppHost/Program.cs
+++ DotNetDataAccessTour.AppHost/Program.cs
@@ -4,9 +4,14 @@ var sqlServer = builder.AddSqlServer("ms-sql")
   .WithLifetime(ContainerLifetime.Persistent)
   .AddDatabase("webdatademo-db");
 
+var seq = builder.AddSeq("seq")
+  .WithLifetime(ContainerLifetime.Persistent);
+
 builder.AddProject<Projects.WebDataDemo>("webdatademo")
   .WithReference(sqlServer)
-  .WaitFor(sqlServer);
+  .WaitFor(sqlServer)
+  .WithReference(seq)
+  .WaitFor(seq); 
 
 builder.AddProject<Projects.MigrationService>("migrationservice")
   .WithReference(sqlServer)
